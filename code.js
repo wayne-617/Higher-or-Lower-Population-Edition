92648382d60a4ae899e828e28a4443b20bceda1e
wayne-617 <wgl91603@uga.edu>
2023-02-04 14:01:08 -0500

    set up IPA

---
+++ code.js
@@ -0,0 +1,38 @@
+let countries;
+
+
+/*fetch("https://restcountries.com/v3.1/all")
+then(function(res){
+    //console.log(res);
+    return res.json();
+})
+.then(function(data){
+    //console.log(data);
+    initialize(data);
+})
+
+function initialize(countriesData){
+    console.log(countriesData)
+} */
+
+fetch("https://restcountries.com/v3.1/all")
+.then(res => res.json())
+.then(data => initialize(data))
+
+function getRandomCountry(total)  {
+    return Math.floor(Math.random() * total);
+
+}
+
+let i = getRandomCountry(249);
+console.log(i);
+
+
+
+function initialize(countriesData)  {
+    countries = countriesData;
+
+    console.log("Population of", countries[i].name.common, "is", countries[i].population);
+}
+
+
