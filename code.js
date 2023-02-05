c639bfecb9a78af827172fea19a883cc45b60c50
wayne-617 <wgl91603@uga.edu>
2023-02-04 19:47:31 -0500

    names and population

--- code.js
+++ code.js
@@ -45,13 +45,15 @@ function initialize(countriesData)  {
     populationA=(countries[a].population);
     nameA=(countries[a].name.common);
     document.getElementById("nameA").innerHTML = nameA;
-    
+    document.getElementById("populationA").innerHTML = "Population: " + populationA;
 
     console.log("Population of", countries[b].name.common, "is", countries[b].population);
     let flagLinkB=(countries[b].flags.svg);
     console.log(flagLinkB);
     document.querySelector("#flag2 img").src = flagLinkB;
     populationB=(countries[b].population);
+    nameB=(countries[b].name.common);
+    document.getElementById("nameB").innerHTML = nameB;
 }
 
 let score = 0
